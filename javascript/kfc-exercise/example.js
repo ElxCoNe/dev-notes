33759bea740a44c0beadaac800a569f3b6dbcba5
Milo <jchernandez10ocm@gmail.com>
2026-01-10 12:06:18 -0500

    add:carsCrud

---
+++ javascript/kfc-exercise/example.js
@@ -0,0 +1,13 @@
+// Grab the welcome-el paragraph and store it in a variable called welcomeEl
+
+let welcomeEl = document.getElementById("welcome-el"); 
+
+
+// Create two variables (name & greeting) that contains your name
+let name = "Milo";
+let greeting = "welcome back";
+// and the greeting we want to render on the page
+
+// Render the welcome message using welcomeEl.innerText
+console.log(welcomeEl.textContent)
+
